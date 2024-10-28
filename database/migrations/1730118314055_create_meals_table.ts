afb1e3652d858ab7e659badbe96e1f9b8b84ba4f
Jakub Czajkowski <and.jj.an@gmail.com>
2024-10-28 16:34:41 +0100

    feat: make migrations

---
+++ database/migrations/1730118314055_create_meals_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'meals'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.text('name')
+      table.enum('type', ['SALAD', 'SOUP', 'VEGETARIAN_DISH', 'MEAT_DISH', 'DESSERT', 'SIDE_DISH'])
+      table.decimal('price', 4, 2)
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.raw('DROP TYPE IF EXISTS "type"')
+    this.schema.dropTable(this.tableName)
+  }
+}
