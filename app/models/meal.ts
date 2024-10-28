5546be3c1c93c94c0daff014b4b6f2e42e820d62
Jakub Czajkowski <and.jj.an@gmail.com>
2024-10-28 16:34:29 +0100

    feat: make models

---
+++ app/models/meal.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export enum MealType {
+  SALAD = 'salad',
+  SOUP = 'soup',
+  VEGETARIAN_DISH = 'vegetarian_dish',
+  MEAT_DISH = 'meat_dish',
+  DESSERT = 'dessert',
+  SIDE_DISH = 'side_dish',
+}
+
+export default class Meal extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare type: MealType
+
+  @column()
+  declare price: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
