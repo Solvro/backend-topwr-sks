675e1332225fb39ef11ee3512dec71367d93a423
Jakub Czajkowski <and.jj.an@gmail.com>
2024-10-29 15:55:53 +0100

    refactor: applied suggestions from PR

--- scripts/scrapper.ts
+++ scripts/scrapper.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import * as cheerio from 'cheerio'
-import Meal, { MealCategory } from '#models/meal'
+import { MealCategory } from '#models/meal'
 
 export async function scrapeMenu() {
   const url = 'https://sks.pwr.edu.pl/menu/'
@@ -8,52 +8,51 @@ export async function scrapeMenu() {
   const response = await axios.get(url)
   const $ = cheerio.load(response.data)
 
-  const menuItems: Partial<Meal>[] = []
+  return $('.category')
+    .map((_, category) => {
+      const categoryName = $(category).find('.cat_name h2').text().trim()
 
-  $('.category').each((_, category) => {
-    const categoryName = $(category).find('.cat_name h2').text().trim()
+      return $(category)
+        .find('.pos ul li')
+        .map((__, item) => {
+          const itemText = $(item).text().trim().replace(/\s+/g, ' ')
+          const price = $(item).find('.price').text().trim()
+          const priceNumeric = Number.parseFloat(price)
 
-    $(category)
-      .find('.pos ul li')
-      .each((__, item) => {
-        const itemText = $(item).text().trim().replace(/\s+/g, ' ')
+          const nameMatch = itemText.match(/[\D\s]+/)
+          const itemName = nameMatch ? nameMatch[0].trim() : itemText
 
-        const price = $(item).find('.price').text().trim()
-        const priceNumeric = Number.parseFloat(price)
+          const sizeMatch = itemText.match(/\d+(?:g|ml)(?:\/\d+(?:g|ml))?/)
+          const itemSize = sizeMatch ? sizeMatch[0].trim() : null
 
-        const nameMatch = itemText.match(/[\D\s]+/)
-        const itemName = nameMatch ? nameMatch[0].trim() : itemText
-
-        const sizeMatch = itemText.match(/\d+(?:g|ml)(?:\/\d+(?:g|ml))?/)
-        const itemSize = sizeMatch ? sizeMatch[0].trim() : null
-
-        menuItems.push({
-          name: itemName,
-          size: itemSize,
-          price: priceNumeric,
-          category: assignCategories(categoryName),
+          return {
+            name: itemName,
+            size: itemSize,
+            price: priceNumeric,
+            category: assignCategories(categoryName),
+          }
         })
-      })
-  })
-
-  return menuItems
+        .get()
+    })
+    .get()
+    .flat()
 }
 
 function assignCategories(category: string) {
-  switch (category) {
-    case 'Surówki':
+  switch (category.toLowerCase()) {
+    case 'surówki':
       return MealCategory.SALAD
-    case 'Zupy':
+    case 'zupy':
       return MealCategory.SOUP
-    case 'Dania jarskie':
+    case 'dania jarskie':
       return MealCategory.VEGETARIAN_DISH
-    case 'Dania mięsne':
+    case 'dania mięsne':
       return MealCategory.MEAT_DISH
-    case 'Dodatki':
+    case 'dodatki':
       return MealCategory.SIDE_DISH
-    case 'Desery':
+    case 'desery':
       return MealCategory.DESSERT
-    case 'Kompoty i napoje':
+    case 'kompoty i napoje':
       return MealCategory.DRINK
     default:
       return null
