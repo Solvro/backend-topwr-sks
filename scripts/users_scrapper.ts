b31c0074cb44e0f90bdb28f7f19e2d06e4fe089d
Jakub Czajkowski <and.jj.an@gmail.com>
2024-11-09 18:44:43 +0100

    feat: add script for scraping users' data

--- scripts/users_scrapper.ts
+++ scripts/users_scrapper.ts
@@ -1,3 +1,45 @@
+import { DateTime } from 'luxon'
+import logger from '@adonisjs/core/services/logger'
+import SksUser from '#models/sks_user'
+
+const url = 'https://live.pwr.edu.pl/sks/sks-data.csv'
+
 export async function runScrapper() {
-  return 'Test'
+  try {
+    const response = await fetch(url)
+    if (!response.ok) {
+      logger.error(`Failed to fetch data: ${response.status} ${response.statusText}`)
+      return
+    }
+    const usersData = await response.text()
+    const currentDateTime = DateTime.now().setZone('Europe/Warsaw')
+    const minutesFromMidnight = currentDateTime.hour * 60 + currentDateTime.minute
+    const updateBatchCount = Math.floor(minutesFromMidnight / 5)
+
+    const rows = usersData.split('\n')
+    const batchRows = rows.slice(0, updateBatchCount)
+
+    for (const row of batchRows) {
+      const [time, movingAverage21, activeUsers] = row.split(';')
+      const timestamp = currentDateTime.set({
+        hour: Number.parseInt(time.split(':')[0], 10),
+        minute: Number.parseInt(time.split(':')[1], 10),
+        second: 0,
+        millisecond: 0,
+      })
+
+      await SksUser.updateOrCreate(
+        {
+          externalTimestamp: timestamp,
+        },
+        {
+          activeUsers: Number(activeUsers),
+          movingAverage21: Number(movingAverage21),
+        }
+      )
+    }
+    logger.info(`Successfully processed and saved ${rows.length} rows of sks_users data.`)
+  } catch (error) {
+    logger.error(`Failed to update sks_users data: ${error.message}`, error.stack)
+  }
 }
