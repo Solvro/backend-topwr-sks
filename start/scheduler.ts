d1e7034ed14355982aba289e80f8945c404db76c
Jakub Czajkowski <and.jj.an@gmail.com>
2024-11-09 18:45:37 +0100

    feat: add script to scheduler

--- start/scheduler.ts
+++ start/scheduler.ts
@@ -1,11 +1,20 @@
 import scheduler from 'adonisjs-scheduler/services/main'
-import { runScrapper } from '../scripts/menu_scrapper.js'
+import { runScrapper as runMenuScrapper } from '../scripts/menu_scrapper.js'
+import { runScrapper as runUsersScrapper } from '../scripts/users_scrapper.js'
 import logger from '@adonisjs/core/services/logger'
 
 scheduler
   .call(() => {
-    logger.info('Running scraper.')
-    void runScrapper()
+    logger.info('Running menu scraper.')
+    void runMenuScrapper()
   })
   .immediate()
-  .everyFifteenMinutes()
+  .everyFiveMinutes()
+
+scheduler
+  .call(() => {
+    logger.info('Running users scraper.')
+    void runUsersScrapper()
+  })
+  .immediate()
+  .everyFiveMinutes()
