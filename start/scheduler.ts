726bfae916ed9e2bc8bd979853b093772f894935
Szymon Kowaliński <szymon@kowalinski.dev>
2025-02-01 16:46:36 +0100

    feat(meals): add enable/disable envs

--- start/scheduler.ts
+++ start/scheduler.ts
@@ -4,11 +4,18 @@ import logger from "@adonisjs/core/services/logger";
 
 import { runScrapper as runMenuScrapper } from "../scripts/menu_scrapper.js";
 import { runScrapper as runUsersScrapper } from "../scripts/users_scrapper.js";
+import env from "./env.js";
 
 scheduler
   .call(() => {
-    logger.info("Running menu scraper.");
-    void runMenuScrapper();
+    if (env.get("RUN_MENU_SCRAPPER")) {
+      logger.info("Running menu scraper.");
+      void runMenuScrapper();
+    } else {
+      logger.info(
+        "Menu scrapper is disabled by the RUN_MENU_SCRAPPER env flag.",
+      );
+    }
   })
   .immediate()
   .everyFifteenMinutes();
@@ -17,8 +24,14 @@ scheduler
 
 scheduler
   .call(() => {
-    logger.info("Running users scraper.");
-    void runUsersScrapper();
+    if (env.get("RUN_USERS_SCRAPPER")) {
+      logger.info("Running users scraper.");
+      void runUsersScrapper();
+    } else {
+      logger.info(
+        "Users scrapper is disabled by the RUN_USERS_SCRAPPER env flag.",
+      );
+    }
   })
   .immediate()
   .everyMinute();
