5c276250d7444d22091b8f39804383be5ccfce61
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2025-02-03 23:30:05 +0100

    fix(migrations): hardcode enum values in migrations
    i predicted this

--- database/migrations/1730118314055_create_meals_table.ts
+++ database/migrations/1730118314055_create_meals_table.ts
@@ -1,16 +1,24 @@
 import { BaseSchema } from "@adonisjs/lucid/schema";
 
-import { MealCategory } from "#models/meal";
-
 export default class extends BaseSchema {
   protected tableName = "meals";
+  protected readonly mealCategories = [
+    "SALAD",
+    "SOUP",
+    "VEGETARIAN_DISH",
+    "MEAT_DISH",
+    "DESSERT",
+    "SIDE_DISH",
+    "DRINK",
+    "TECHNICAL_INFO",
+  ];
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.bigIncrements("id");
 
       table.text("name").notNullable();
-      table.enum("category", Object.keys(MealCategory), {
+      table.enum("category", this.mealCategories, {
         useNative: true,
         enumName: "meal_category",
         existingType: false,
